494af26618a27c183865218ea80a29b26fa20dc8
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-07-06 18:21:16 +0300

    Fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ Video.create(title: "The Shield", description: "Good police show!", small_cover_
 Video.create(title: "Curb Your Enthusiasm", description: "Larry David presents life as himself.", small_cover_url: "/tmp/curbyourenthusiasm_small.jpg", large_cover_url: "/tmp/curbyourenthusiasm_large.jpg", category: comedy)
 Video.create(title: "The Shield", description: "Good police show!", small_cover_url: "/tmp/theshield_small.jpg", large_cover_url: "/tmp/theshield_large.jpg", category: drama)
 monk = Video.create(title: "Monk", description: "A show about stuff", category: drama)
-kevin = User.create(full_name: "Kevin Wang", password: "password", email: "kevin@example.com", email_confirmation: "kevin@example.com")
+kevin = User.create(full_name: "Kevin Wang", password: "password", email: "kevin@example.com")
 
 Review.create(user: kevin, video: monk, rating: 4, content: "What a great movie!")
 Review.create(user: kevin, video: monk, rating: 1, content: "Not my cup of tea really.")
