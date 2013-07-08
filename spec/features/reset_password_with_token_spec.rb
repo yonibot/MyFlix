3b451df0ecb51584d6e28eba4d657face9ecc7f5
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-07-08 15:27:17 +0300

    Finished Mod9-HW1

--- spec/features/reset_password_with_token_spec.rb
+++ spec/features/reset_password_with_token_spec.rb
@@ -2,14 +2,27 @@ require 'spec_helper'
 
 feature 'User resets password using a token' do
   scenario 'User submits password in Forgot Email page and resets password' do
+    clear_emails
 
     alice = Fabricate(:user, email: "alice@example.com", password: "foobarbaz")
     visit login_path
     click_link "Forgot password?"
+
     fill_in "email", with: "alice@example.com"
     click_button "Send Email"
-    open_email('alice@example.com')
-    expect(current_email).to have_content('Someone at this')
+
+    open_email(alice.email)
+    current_email.click_link "Reset my password"
+    fill_in "password", with: "fishsoup"
+    click_on "Reset Password"
+    expect(page).to have_content("has been changed")
+
+    # Sign in user with new password
+    
+    fill_in "Email Address", with: alice.email
+    fill_in "Password", with: "fishsoup"
+    click_on "Sign in"
+    expect(page).to have_content("You are signed in, enjoy!")
 
   end
 end
