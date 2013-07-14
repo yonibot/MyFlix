b1986f1383523ee1dfbfe040a7cfac07ddab0eca
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-07-14 13:49:32 +0300

    amended production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,14 @@ Myflix::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { host: 'limitless-thicket-1567.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'limitless-thicket-1567.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'], 
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'limitless-thicket-1567.herokuapp.com/',
+  :domain         => 'limitless-thicket-1567.herokuapp.com',
   :authentication => :plain,
   }
 
