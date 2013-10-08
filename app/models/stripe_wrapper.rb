142383c0e2a3f0fab7e051abeda1a327359a3513
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-10-08 13:51:58 +0300

    Create customer subscription

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -27,9 +27,32 @@ module StripeWrapper
     def successful?
       response.present?
     end
+  end
 
-    def error_message
-      @error_message
+  class Customer
+
+    attr_reader :error_message, :response
+
+    def initialize(options={})
+      @response = options[:response]
+      @error_message = options[:error_message]
+    end
+
+    def self.create(options={})
+      begin
+      response = Stripe::Customer.create(
+        card: options[:card],
+        email: options[:user].email,
+        plan: "base"
+        )
+      new(response: response)
+      rescue Stripe::CardError => e
+        new(error_message: e.message)
+      end
+    end
+
+    def successful?
+      response.present?
     end
 
 
